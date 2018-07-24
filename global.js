67ff750d26d5f1896eaff675ae15824a7db7bd27
Tai Bui Tan <venkug@sg.uob>
2018-07-24 17:48:59 +0800

    Add code

---
+++ global.js
@@ -0,0 +1,8 @@
+const GLOBAL_CONFIG = {
+    //API_PATH: "http://localhost:9090",
+    API_PATH: "http://pibtsg95.sg.uobnet.com:7098/creditcards",
+    formConfig: "./form.json",
+    formConfigID: "./formID.json",
+    message: "./msg.json",
+    messageID: "./msgID.json"
+};

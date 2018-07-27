8ec2c9ba53cbf58f9d89b1c035bc02ee5d1cd97a
Tai Bui Tan <venkug@sg.uob>
2018-07-27 10:24:11 +0800

    Add code

--- global.js
+++ global.js
@@ -1,8 +1,13 @@
-const GLOBAL_CONFIG = {
-    //API_PATH: "http://localhost:9090",
-    API_PATH: "http://pibtsg95.sg.uobnet.com:7098/creditcards",
-    formConfig: "./form.json",
-    formConfigID: "./formID.json",
-    message: "./msg.json",
-    messageID: "./msgID.json"
+// eslint-disable-next-line no-unused-vars
+const FORMS_CNFG_GLBL = {
+   // WS_PATH: "http://localhost:9090/Apply/api",
+   // SERVER_PATH: "http://localhost:9090/api",
+    WS_PATH: "/api",
+     SERVER_PATH: "http://localhost:9090/api",
+    imageContext: "https://u-uobforms.uob.com.sg/assets/images/web/apply/",
+    WHITELIST_CHARS: "([^A-Z a-z0-9@/*#.&=$%?!;,:()`+'_-])",
+    DEFAULT_LANGUAGE: "en",
+    formConfig: "/form.json",
+    productConfig: "/productConfig.json",
+    imgPath: "/images"
 };
---
+++ myinfo.js
@@ -0,0 +1,26 @@
+(function() {
+    try {
+        var viz = document.createElement("script");
+        viz.type = "text/javascript";
+        viz.async = true;
+        viz.src = ("https:" == document.location.protocol ?"https://sea-tags.vizury.com" : "http://sea-tags.vizury.com")+ "/analyze/pixel.php?account_id=VIZVRM5577";
+
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(viz, s);
+        viz.onload = function() {
+            try {
+                pixel.parse();
+            } catch (i) {
+            }
+        };
+        viz.onreadystatechange = function() {
+            if (viz.readyState == "complete" || viz.readyState == "loaded") {
+                try {
+                    pixel.parse();
+                } catch (i) {
+                }
+            }
+        };
+    } catch (i) {
+    }
+})();

ae0df3e20d83763d6f1ac96fabfb61688d2bf30b
Tai Bui Tan <venkug@sg.uob>
2018-08-10 15:47:41 +0800

    update code

--- global.js
+++ global.js
@@ -1,13 +1,8 @@
-// eslint-disable-next-line no-unused-vars
-const FORMS_CNFG_GLBL = {
-   // WS_PATH: "http://localhost:9090/Apply/api",
-   // SERVER_PATH: "http://localhost:9090/api",
-    WS_PATH: "/api",
-     SERVER_PATH: "http://localhost:9090/api",
-    imageContext: "https://u-uobforms.uob.com.sg/assets/images/web/apply/",
-    WHITELIST_CHARS: "([^A-Z a-z0-9@/*#.&=$%?!;,:()`+'_-])",
-    DEFAULT_LANGUAGE: "en",
-    formConfig: "/form.json",
-    productConfig: "/productConfig.json",
-    imgPath: "/images"
+const GLOBAL_CONFIG = {
+    //API_PATH: "http://localhost:9090",
+    API_PATH: "http://pibtsg95.sg.uobnet.com:7098/creditcards",
+    formConfig: "./form.json",
+    formConfigID: "./formID.json",
+    message: "./msg.json",
+    messageID: "./msgID.json"
 };

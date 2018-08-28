9ac31639b7b47f6caefcdc036017e5575d89c297
Tai Bui Tan <venkug@sg.uob>
2018-08-28 10:02:45 +0800

    update

--- global.js
+++ global.js
@@ -1,8 +1,12 @@
-const GLOBAL_CONFIG = {
-    //API_PATH: "http://localhost:9090",
-    API_PATH: "http://pibtsg95.sg.uobnet.com:7098/creditcards",
-    formConfig: "./form.json",
-    formConfigID: "./formID.json",
-    message: "./msg.json",
-    messageID: "./msgID.json"
+// eslint-disable-next-line no-unused-vars
+const FORMS_CNFG_GLBL = {
+    WS_PATH: "/bundle/api",
+    SERVER_PATH: "/api",
+    imageContext: "https://u-uobforms.uob.com.sg/assets/images/web/apply/",
+    WHITELIST_CHARS: "([^A-Z a-z0-9@/*#.&=$%?!;,:()`+'_-])",
+    DEFAULT_LANGUAGE: "en",
+    banks: "/bundle/static/banks.json",
+    formConfig: "/bundle/static/form.json",
+    productConfig: "/bundle/static/productConfig.json",
+    imgPath: "/bundle/static/images"
 };

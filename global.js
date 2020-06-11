623993ac705c14136a0c00d029efee0d2ad0fe2c
Tai Bui Tan <venbut@sg.uob>
2020-06-11 10:58:27 +0800

    Update code

--- global.js
+++ global.js
@@ -1,12 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-const FORMS_CNFG_GLBL = {
-    WS_PATH: "/bundle/api",
-    SERVER_PATH: "/api",
-    imageContext: "https://u-uobforms.uob.com.sg/assets/images/web/apply/",
-    WHITELIST_CHARS: "([^A-Z a-z0-9@/*#.&=$%?!;,:()`+'_-])",
-    DEFAULT_LANGUAGE: "en",
-    banks: "/bundle/static/banks.json",
-    formConfig: "/bundle/static/form.json",
-    productConfig: "/bundle/static/productConfig.json",
-    imgPath: "/bundle/static/images"
+const GLOBAL_CONFIG = {
+    API_PATH: "./api/v1/property/loan/agent",
+    COMMON_API_PATH: "./api/sg/v1",
+    BANKER_API_PATH: "https://api.sg.uobagents.com/2.0/partner/banker",
+    MYINFO_URL: "/dummysingpass.html?redirect_uri=http://pibtsg95:6507/camelcar/callback&client_id=stg_uob_one_casacc&purpose=opening an account online with UOB&attributes=aliasname,assessableincome,birthcountry,dob,edulevel,email,employment,hanyupinyinaliasname,hanyupinyinname,hdbtype,housingtype,marriedname,marital,mobileno,name,nationality,regadd,sex&state=",
+    formConfig: "./form.json",
+    bankerList: "./bankerList.json",
+    dashboardDetails: "./dashboardDetails.json",
+    localEnvironment: true,
+    timeout: "45000",
+    key: "cae2888659a3fbc22422895fd71c4fa2",
+    agencyCookie: "property-source-agent"
 };
